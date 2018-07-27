946b36c022a47cedd2a5f4327ddc9768347969e1
Alexander Ji <alexji@mit.edu>
2018-07-27 15:19:29 -0700

    Fixed import

--- alexmods/infer_distance.py
+++ alexmods/infer_distance.py
@@ -9,7 +9,7 @@ from astropy import units as u
 
 from pyia import GaiaData
 from importlib import reload
-import gaiatools as gtool; reload(gtool)
+import .gaiatools as gtool; reload(gtool)
 
 default_L = 0.5
 
