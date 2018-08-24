9110512070552f79cf5f028519e14f70a10b9c34
Alexander Ji <alexji@mit.edu>
2018-08-24 00:40:34 -0700

    Added a (literally completely untested) peak finding program

--- alexmods/utils.py
+++ alexmods/utils.py
@@ -8,6 +8,7 @@ from __future__ import (division, print_function, absolute_import,
 from six import string_types
 import numpy as np
 from .robust_polyfit import gaussfit
+from scipy import interpolate
 
 def struct2array(x):
     """ Convert numpy structured array of simple type to normal numpy array """
@@ -34,30 +35,141 @@ def air2vac(lamair):
     n = 1 + 0.00008336624212083 + 0.02408926869968 / (130.1065924522 - s2) + 0.0001599740894897 / (38.92568793293 - s2)
     return lamair*n
 
-def find_distribution_peak(x, x0, s0=None, bins='auto'):
-    """
-    Take a histogram of the data and find the location of the peak closest to x0
-    x: data (no nan's allowed)
-    x0: peak location guess
-    s0: width of peak guess (default std(x))
-    """
-    h, x = np.histogram(x, bins=bins)
-    x = (x[1:]+x[:-1])/2.
-    # find positive peak locations based on derivative
-    dh = np.gradient(h)
-    peaklocs = np.where((dh[:-1] > 0) & (dh[1:] < 0))[0]
-    if len(peaklocs)==0:
-        raise ValueError("No peaks found!")
-    # get the best peak
-    xpeaks = x[peaklocs+1]
-    bestix = np.argmin(np.abs(xpeaks - x0))
-    xbest = x[bestix]
-    ybest = h[bestix]
-    if s0 is None:
-        s_est = np.std(x)
-    else:
-        s_est = s0
-    # fit a Gaussian and get the peak
-    A, xfit, s = gaussfit(x, h, [ybest, xbest, s_est], maxfev=99999)
-    return xfit
+##def find_distribution_peak(x, x0, s0=None, bins='auto'):
+##    """
+##    Take a histogram of the data and find the location of the peak closest to x0
+##    x: data (no nan's allowed)
+##    x0: peak location guess
+##    s0: width of peak guess (default std(x))
+##    """
+##    h, x = np.histogram(x, bins=bins)
+##    x = (x[1:]+x[:-1])/2.
+##    # find positive peak locations based on derivative
+##    dh = np.gradient(h)
+##    peaklocs = np.where((dh[:-1] > 0) & (dh[1:] < 0))[0]
+##    if len(peaklocs)==0:
+##        raise ValueError("No peaks found!")
+##    # get the best peak
+##    xpeaks = x[peaklocs+1]
+##    bestix = np.argmin(np.abs(xpeaks - x0))
+##    xbest = x[bestix]
+##    ybest = h[bestix]
+##    if s0 is None:
+##        s_est = np.std(x)
+##    else:
+##        s_est = s0
+##    # fit a Gaussian and get the peak
+##    A, xfit, s = gaussfit(x, h, [ybest, xbest, s_est], maxfev=99999)
+##    return xfit
 
+def get_cdf_raw(x):
+    """
+    Take a set of points x and find the CDF.
+    Returns xcdf, ycdf, where ycdf = p(x < xcdf)
+    Normalized such that p(min(x)) = 1/len(x), p(max(x)) = 1
+    """
+    xcdf = np.argsort(x)
+    ycdf = np.arange(len(x)).astype(float)/float(len(x))
+    return xcdf, ycdf
+
+def get_cdf(x, **kwargs):
+    """
+    Take a set of points x and find the CDF.
+    Use get_cdf_raw, fit and return an interpolating function.
+    
+    By default we use scipy.interpolate.UnivariateSpline.
+    kwargs are passed to the interpolating function.
+    (We do not fit a perfectly interpolating spline right now, because if two points have identical x's,
+    you get infinity for the derivative.)
+    Note: scipy v1 has a bug in all splines right now that rejects any distribution of points with exactly equal x's.
+    
+    You can obtain the PDF by taking the first derivative.
+    """
+    xcdf, ycdf = get_cdf_raw(x)
+    if "ext" not in kwargs:
+        kwargs["ext"] = 3 #return the boundary value rather than extrapolating
+    F = interpolate.UnivariateSpline(xcdf, ycdf, **kwargs)
+    return F
+
+def find_distribution_peak(x, xtol, full_output=False):
+    """
+    Finds largest peak from an array of real numbers (x).
+    
+    Algorithm:
+    - Compute the CDF by fitting cubic smoothing spline
+    - find PDF with derivative (2nd order piecewise poly)
+    - Sample the PDF at xtol/2 points
+    - Find peaks in the PDF as the maximum points
+    - Return the biggest PDF peak
+    """
+    Fcdf = get_cdf(x)
+    fpdf = F.derivative()
+    
+    xsamp = np.arange(np.min(x), np.max(x)+xtol, xtol/2.)
+    pdf = fpdf(xsamp)
+    ix = np.argmax(pdf)
+    xpeak = xsamp[ix]
+    
+    if full_output:
+        return xpeak, xsamp, pdf
+    else:
+        return xpeak
+    
+def find_confidence_region(x, p, xtol, full_output=False):
+    """
+    Finds smallest confidence region from an array of real numbers (x).
+    
+    Algorithm:
+    - use find_distribution_peak to get the peak value and pdf
+      The pdf is uniformly sampled at xtol/2 from min(x) to max(x)
+    - initialize two tracers x1 and x2 on either side of the peak value
+    - step x1 and x2 down (by xtol/2), picking which one locally adds more to the enclosed probability
+    
+    Note this does not work so well with multimodal things.
+    It is also pretty slow, and not so accurate (does not interpolate the PDF or do adaptive stepping)
+    """
+    assert (p > 0) and (p < 1)
+    xpeak, xsamp, pdf = find_distribution_peak(x, xtol, full_output=True)
+    
+    pdf = pdf / np.sum(pdf) # normalize this to 1
+    
+    ## Initialize the interval 
+    ipeak = np.argmin(np.abs(xsamp-xpeak))
+    i1 = ipeak-1; i2 = ipeak+1
+    p1 = pdf[i1]; p2 = pdf[i2]
+    current_prob = pdf[ipeak]
+    def step_left():
+        current_prob += p1
+        i1 -= 1
+        p1 = pdf[i1]
+    def step_right():
+        current_prob += p2
+        i2 += 1
+        p2 = pdf[i2]
+        
+    ## Expand the interval until you get enough probability
+    while current_prob < p:
+        # If you reached the end, expand the left/right until you're done
+        if i1 <= 0:
+            while current_prob < p:
+                step_right()
+            break
+        # If you reached the end, expand the left until you're done
+        if i2 >= len(pdf):
+            while current_prob < p:
+                step_left()
+            break
+        # Step in the direction
+        if p1 > p2:
+            step_left()
+        elif p1 < p2:
+            step_right()
+        else: # Pick a direction at random if exactly equal
+            if np.random.random() > 0.5:
+                step_right()
+            else:
+                step_left()
+    if full_output:
+        return xsamp[i1], xpeak, xsamp[i2], current_prob, xsamp, pdf
+    return xsamp[i1], xpeak, xsamp[i2]
+            
