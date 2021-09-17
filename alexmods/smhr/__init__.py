3391033ca9c0cf2c075c06fe52be14c257e61b94
Alex Ji <alexji@uchicago.edu>
2021-09-17 16:50:00 -0500

    Added a quick synthesizer; still a pain to use but better than nothing

--- alexmods/smhr/__init__.py
+++ alexmods/smhr/__init__.py
@@ -6,6 +6,7 @@ from __future__ import (division, print_function, absolute_import,
 
 try:
     from . import radiative_transfer as rt
-except:
+except Exception as e:
     print("Problem with rt, will not work")
+    print(e)
 from . import photospheres
